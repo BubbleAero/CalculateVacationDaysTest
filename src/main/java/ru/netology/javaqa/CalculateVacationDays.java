c9ca228f248ed4351d962b295a17c254eb5e799b
NetoTest <vikavasilenko2013@gmail.com>
2024-08-12 21:05:17 +0300

    Changes in formula

--- src/main/java/ru/netology/javaqa/CalculateVacationDays.java
+++ src/main/java/ru/netology/javaqa/CalculateVacationDays.java
@@ -4,10 +4,9 @@ public class CalculateVacationDays {
     public int calculate(int income, int expenses, int threshold) {
         int money = 0; // сколько денег
         int monthOfRest = 0; // сколько месяцев отдыха
-        for (int month = 1; month < 12; month++) {
+        for (int month = 1; month <= 12; month++) {
             if (money >= threshold) { // можем отдыхать
-                money = money - expenses; // что осталось после расходов
-                money = money - (money/3);//траты на отдых
+                money = (money - expenses) - (money - (money/3)); // что осталось после расходов
                 monthOfRest++;
             } else {
                 money = money + income - expenses;

881e1a6c894c5412459bfa603c44189c1aa02cd2
NetoTest <vikavasilenko2013@gmail.com>
2024-08-13 11:27:05 +0300

    New Formula

--- src/main/java/ru/netology/javaqa/CalculateVacationDays.java
+++ src/main/java/ru/netology/javaqa/CalculateVacationDays.java
@@ -4,9 +4,9 @@ public class CalculateVacationDays {
     public int calculate(int income, int expenses, int threshold) {
         int money = 0; // сколько денег
         int monthOfRest = 0; // сколько месяцев отдыха
-        for (int month = 1; month <= 12; month++) {
+        for (int month = 1; month < 12; month++) {
             if (money >= threshold) { // можем отдыхать
-                money = (money - expenses) - (money - (money/3)); // что осталось после расходов
+                money = (money - expenses)/3; // что осталось после расходов
                 monthOfRest++;
             } else {
                 money = money + income - expenses;
